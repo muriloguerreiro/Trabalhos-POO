54f6f1b32262b7eee28662de76a653e611738b71
Murilo Guerreiro <guerreiro_murilo@hotmail.com>
2019-05-24 14:48:58 -0300

    Atualizando trabalhos em 24/05/2019

---
+++ Trabalho de POO 6/wfaArquivoTexto/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace wfaArquivoTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbEntrada = new System.Windows.Forms.TextBox();
+            this.tbSaida = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Entre arquivo ou diretório:";
+            // 
+            // tbEntrada
+            // 
+            this.tbEntrada.Location = new System.Drawing.Point(44, 80);
+            this.tbEntrada.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbEntrada.Name = "tbEntrada";
+            this.tbEntrada.Size = new System.Drawing.Size(352, 22);
+            this.tbEntrada.TabIndex = 1;
+            this.tbEntrada.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbEntrada_KeyDown);
+            // 
+            // tbSaida
+            // 
+            this.tbSaida.Location = new System.Drawing.Point(44, 132);
+            this.tbSaida.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbSaida.Multiline = true;
+            this.tbSaida.Name = "tbSaida";
+            this.tbSaida.Size = new System.Drawing.Size(352, 402);
+            this.tbSaida.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(436, 162);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 66);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "GRAVAR ARQUIVO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 487);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbSaida);
+            this.Controls.Add(this.tbEntrada);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Busca de Arquivo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbEntrada;
+        private System.Windows.Forms.TextBox tbSaida;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Trabalho de POO 6/wfaArquivoTexto/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+// Murilo Guerreiro Badoco 11711ECP010
+
+namespace wfaArquivoTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Obtem informação sobre arquivo ou diretório
+        private string obtemInformacaoArquivo(string nomeArquivo)
+        {
+            string informacao;
+
+            // saída dizendo que o arquivo ou diretório existe
+            informacao = nomeArquivo + " existe\r\n\r\n";
+
+            // saída dizendo quando o arquivo ou diretório foi criado
+            informacao += "Criado em : " +
+               File.GetCreationTime(nomeArquivo) + "\r\n";
+
+            // saída dizendo quando o arquivo ou diretório foi modificado pela última vez
+            informacao += "Última modificação: " +
+               File.GetLastWriteTime(nomeArquivo) + "\r\n";
+
+            // saída dizendo quando o arquivo ou diretório foi acessado pela última vez
+            informacao += "Último acesso: " +
+               File.GetLastAccessTime(nomeArquivo) + "\r\n" + "\r\n";
+
+            return informacao;
+
+        } // fim do método obtemInformacaoArquivo
+
+        // chamado quando o usuário pressiona uma tecla
+        private void tbEntrada_KeyDown(object sender, KeyEventArgs e)
+        {
+            // determina se o usuário pressionou a tecla Enter
+            if (e.KeyCode == Keys.Enter)
+            {
+                string nomeArquivo; // nome do arquivo ou diretório
+
+                // pega arquivo ou diretório especificado pelo usuário
+                nomeArquivo = tbEntrada.Text;
+
+                // determina se nomeArquivo é um arquivo
+                if (File.Exists( nomeArquivo ))
+                {
+                    // obtenha a data de criação do arquivo, 
+                    // data de modificação, etc.
+                    tbSaida.Text = obtemInformacaoArquivo(nomeArquivo);
+
+                     // exibe o conteúdo do arquivo por meio de StreamReader
+                    try
+                    {
+                        // obtém o Reader e o conteúdo do arquivo
+                        StreamReader stream = new StreamReader(nomeArquivo);
+                        tbSaida.Text += stream.ReadToEnd();
+                    }
+                    // trata exceção se StreamReader está indisponível
+                    catch (IOException)
+                    {
+                        MessageBox.Show("Erro de Arquivo", "Erro de Arquivo",
+                           MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                } // fim do (File.Exists( nomeArquivo ))
+
+                // determine se nomeArquivo é um diretório
+                else if (Directory.Exists(nomeArquivo))
+                {
+                    // array para diretórios
+                    string[] directoryList;
+
+                    // obtém data de criação do diretório, 
+                    // data de modificação, etc.
+                    tbSaida.Text = obtemInformacaoArquivo(nomeArquivo);
+
+                    // obtém a lista de arquivos e diretórios
+                    directoryList = Directory.GetDirectories(nomeArquivo);
+
+                    tbSaida.Text +=
+                       "\r\n\r\nConteúdo do diretório:\r\n";
+
+                    // exibe conteúdo de directoryList 
+                    for (int i = 0; i < directoryList.Length; i++)
+                        tbSaida.Text += directoryList[i] + "\r\n";
+                }
+                else
+                {
+                    // notifica usuário quando não existe arquivo ou diretório
+                    MessageBox.Show(tbEntrada.Text +
+                       " não existe", "Erro de Arquivo",
+                       MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            } // end if
+
+        } // fim do método tbEntrada_KeyDown
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            {
+                string caminho = tbEntrada.Text;
+                // Se o arquivo não existir, é salvado apenas o texto 
+                // escolhido.
+            if (File.Exists(caminho))
+               {
+                    try
+                    {
+                        tbSaida.SelectionStart = tbEntrada.Text.Length + 123 + File.ReadAllText(caminho).Length;
+                        tbSaida.SelectionLength = tbSaida.Text.Length;
+                        File.AppendAllText(caminho, tbSaida.SelectedText);
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Houve um erro. Tente novamente.", "Erro");
+                    }
+               }
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+                string caminho = tbEntrada.Text;
+                if (File.Exists(caminho))
+                {
+                    tbSaida.SelectionStart = tbEntrada.Text.Length + 123;
+                    tbSaida.SelectionLength = tbSaida.Text.Length;
+                    if (tbSaida.SelectedText.Equals(File.ReadAllText(caminho)))
+                    {
+                    }
+                    else
+                    {
+                        DialogResult resposta = MessageBox.Show("Deseja salvar as alterações?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (resposta == System.Windows.Forms.DialogResult.Yes)
+                        {
+                            try
+                            {
+                                tbSaida.SelectionStart = tbEntrada.Text.Length + 123 + File.ReadAllText(caminho).Length;
+                                tbSaida.SelectionLength = tbSaida.Text.Length;
+                                File.AppendAllText(caminho, tbSaida.SelectedText);
+                            }
+                            catch (Exception)
+                            {
+                                MessageBox.Show("Houve um erro. Tente novamente.", "Erro");
+                            }
+                        }
+                    }
+                }
+            
+        }
+    } // fim da classe class Form1 : Form
+
+} // fim do namespace wfaArquivoTexto
+
+
