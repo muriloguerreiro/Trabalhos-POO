62650490105f1fbc7740ee1493f677bb8960e423
Murilo Guerreiro <guerreiro_murilo@hotmail.com>
2019-05-23 21:36:15 -0300

    Adicionando trabalho

---
+++ Trabalho de POO 1/POO_Lab_01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Murilo Guerreiro Badoco 11711ECP010 */
+
+namespace POO_Lab_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vetor2D VetorA = new Vetor2D(3.0, 7.0);
+            Vetor2D VetorB = new Vetor2D(2.0, 4.0);
+            Console.WriteLine("Produto Escalar: " + VetorA.Produto_Escalar(VetorB));
+            Console.WriteLine("Modulo do VetorA: " + VetorA.Modulo_do_Vetor());
+            Console.WriteLine("Modulo do VetorB: " + VetorB.Modulo_do_Vetor());
+            Console.WriteLine("Angulo Entre os Dois Vetores: " + VetorA.Angulo_Dois_Vetores(VetorB) + " graus");
+            Vetor2D VetorProj = new Vetor2D();
+            VetorProj = VetorA.Vetor_Projecao(VetorB);
+
+            Console.WriteLine("Projeção do Vetor A no Vetor B: (" + VetorProj.Getx() + " , " + VetorProj.Gety() + ")");
+            VetorProj = VetorB.Vetor_Projecao(VetorA);
+            Console.WriteLine("Projeção do Vetor B no Vetor A: (" + VetorProj.Getx() + " , " + VetorProj.Gety() + ")");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Trabalho de POO 1/POO_Lab_01/Vetor2D.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Lab_01
+{
+    class Vetor2D
+    {
+        private double x;
+        private double y;
+
+        /*     a)     */
+
+        public Vetor2D()
+        {
+            x = 0;
+            y = 0;
+        }
+        public Vetor2D(double X,double Y)
+        {
+            x = X;
+            y = Y;
+        }
+
+        /*     b)     */
+
+        public double Getx()
+        {
+            return this.x;
+        }
+        public void Setx(double X)
+        {
+            x = X;
+        }
+        public double Gety()
+        {
+            return this.y;
+        }
+        public void Sety(double Y)
+        {
+            y = Y;
+        }
+
+        /*     c)     */
+
+        public double Produto_Escalar(Vetor2D vetor)
+        {
+            return (x * vetor.Getx() + y * vetor.Gety());
+        }
+
+        /*     d)     */
+
+        public double Modulo_do_Vetor()
+        {
+            return Math.Sqrt(Math.Pow(x, 2.0) + Math.Pow(y, 2.0));
+        }
+
+        /*     e)     */
+
+        public double Angulo_Dois_Vetores(Vetor2D vetor)
+        {
+            double cos;
+            cos = (Produto_Escalar(vetor) / (Modulo_do_Vetor() * vetor.Modulo_do_Vetor()));
+            return (Math.Acos(cos) * 180 / Math.PI);
+        }
+
+        /*     f)     */
+
+        public Vetor2D Vetor_Projecao(Vetor2D vetor)
+        {
+            double proj;
+            Vetor2D vetorprojecao = new Vetor2D();
+            proj = (Produto_Escalar(vetor) / Produto_Escalar(this));
+            vetorprojecao.Setx(Getx() * proj);
+            vetorprojecao.Sety(Gety() * proj);
+            return vetorprojecao;
+        }
+    }
+}
